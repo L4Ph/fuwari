a7e02b717675e88c3f068ff7763fb2b7a3d6271a
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-12-06 15:32:09 +0900

    Switch to Content Layer API and be able to display articles

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content'
+import { file, glob } from 'astro/loaders'
 
 const postsCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/posts' }),
   schema: z.object({
     title: z.string(),
     published: z.date(),
@@ -19,6 +21,12 @@ const postsCollection = defineCollection({
     nextSlug: z.string().default(''),
   }),
 })
+
+const specCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.md', base: './src/content/spec' }),
+})
+
 export const collections = {
   posts: postsCollection,
+  spec: specCollection,
 }
